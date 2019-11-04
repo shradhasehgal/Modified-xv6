51cc48dbb4f3d10424ff9ce32631535b50aff65b
Shradha <shradha.sgl@gmail.com>
2019-11-04 16:17:18 +0530

    Mostly working

--- time.c
+++ time.c
@@ -3,48 +3,74 @@
 #include "user.h"
 #include "fs.h"
 
-#define ll long long int
-#define MAX 100000000
+#define ll volatile long long int
+#define MAX 10000000
 
-int main (int argc,char *argv[])
+// int main (int argc,char *argv[])
+// {
+//   int pid, status = 0, a = 0 ,b = 0, oof=0;
+//   pid = fork();
+//   if (pid == 0)
+//   {	
+//       ll x = 0, y;
+//       for (y = 0; y < MAX; y++)
+//         x = 1 ^ x; 
+//       exit();
+//       // exec(argv[1], argv);
+//       // printf(1, "exec %s failed\n", argv[1]);
+//       // exit();
+//   }
+
+//   else
+//   {
+//     status = waitx(&a,&b);
+//   }  
+//   printf(1, "Wait Time = %d\nRun Time = %d with Status %d \n",a,b,status); 
+//   exit();
+// }
+
+
+int main(int argc, char** argv) 
 {
-  int pid;
-	int status = 0, a, b;
-	pid = fork();
-	if (pid == 0)
-	{
-		long long int z, x = 0;
-		printf(1, "I'm Child Process\n");
-		for (z = 0; z < 2147483648; z += 1)
-			x = x + x; // useless calculations to consume CPU time
-		exit();
-	}
-	else
-	{
-		status = waitx(&a, &b);
-		printf(1, "Wait Time = %d\nRun Time = %d with Status %d \n", a, b, status);
-	}
+  int wtime, rtime, status = 0;
+  int pid = fork();
+  if (pid < 0) 
+  {
+      printf(2, "Failed to fork\n");
+      exit();
+  } 
+  
+  else if (pid == 0) 
+  {
+    if(argc == 1)
+    {
+      printf(1, "Timing default program\n");
+      ll x = 0, y;
+      for (y = 0; y < MAX; y++)
+        x = 1 ^ x; 
+      exit();
+    }
+    else 
+    {
+      printf(1, "Timing %s\n", argv[1]);
+      if (exec(argv[1], argv + 1) < 0) 
+      {
+          printf(2, "exec %s failed\n", argv[1]);
+          exit();
+      }
+    }
+  }
 
-	exit();
-  // int pid, status = 0, a = 0 ,b = 0;
-  	
+  else if (pid > 0) 
+  {
+    status = waitx(&wtime, &rtime);
+    if(argc == 1)
+    {
+      printf(1, "Time taken by default process\nWait time: %d\nRun time: %d with Status %d\n\n",wtime, rtime, status);  
+    }
 
-  // pid = fork();
-  // if (pid == 0)
-  // {	
-  //     ll x = 0, y;
-  //     for (y = 0; y < MAX; y++)
-  //       x = 1 ^ x; 
-  //     exit();
-  //     // exec(argv[1], argv);
-  //     // printf(1, "exec %s failed\n", argv[1]);
-  //     // exit();
-  // }
+    else printf(1, "Time taken by %s\nWait time: %d\nRun time: %d with Status %d\n\n", argv[1],wtime, rtime, status);
+    exit();
+  }
 
-  // else
-  // {
-  //   status = waitx(&a,&b);
-  // }  
-  // printf(1, "Wait Time = %d\nRun Time = %d with Status %d \n",a,b,status); 
-  // exit();
 }
