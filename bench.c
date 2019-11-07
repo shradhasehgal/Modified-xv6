e3d080fc44a80d79661a4c1dba48b4f725bfa9f0
Shradha <shradha.sgl@gmail.com>
2019-11-07 22:19:34 +0530

    Bench file added

--- bench.c
+++ bench.c
@@ -6,61 +6,41 @@
 #include "pstat.h"
 
 #define vll volatile long long int
-#define MAX 5000000
+#define MAX 100000000
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	for(int i=0; i < 10; i++)
-    {
-        printf(1, "Process %d\n", i);
-        int pid = fork();
-        if (pid < 0)
-        {
-            printf(2, "Failed to fork\n");
-            exit();
-        }
+	for (int i = 0; i < 10; i++)
+	{
+		int pid = fork();
 
-        if(pid == 0)
-        {
-            #ifdef PBS
-                int my_pid = getpid();
-                set_priority(my_pid, (i*10+17)%100);
-                //set_priority(my_pid, 10);
-            #endif
+		if (pid < 0)
+		{
+			printf(2, "Failed to fork\n");
+			exit();
+		}
 
-            int f = 0;
-            for(int j=0; j < 5; j++)
-            {
-                if(f==0)
-                {
-                    sleep(i*10 + j +100);
-                    f=1;
-                }
-                
-                else
-                {
-                    f = 0;
-                    for(int k=0; k < j+ 5 ;k++)
-                    {
-                        vll x = 0, y;
-                        for (y = 0; y < MAX; y++)
-                            x = 1 ^ x; //cpu time
-                    }
-                    
-                }
+		else if (pid == 0)
+		{
+			#ifdef PBS
+				set_priority(getpid(), 107 - 9 * i);
+			#endif
+			
+			sleep(200*i + 200); //io
+		
+			for(vll l=0; l < 10-i; l++)
+			{
+				for (vll k = 0; k < MAX; k++)
+					k = 1 ^ k; //cpu
+			}
+			exit();
+		}
+	}
+	for (int i = 0; i < 10; i++)
+		wait();
 
-            }
-            // printf("noice");
-            exit();
-
-        } 
-    }
-
-    for (int i = 0; i < 10; i++)
-        wait();
-
-    struct proc_stat p; 
-    int pid = getpid();
+	struct proc_stat p;
+	int pid = getpid();
     getpinfo(&p, pid);
     printf(1, "Process with PID=%d has\nRunTime=%d\nNum_Run=%d\n",p.pid, p.runtime, p.num_run);
 
